3016d8e70be6a6bf99078d7406b01ca7d068dfa3
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-02-12 11:27:19 -0500

    Commenting out the suspenders for current run (to be fixed later)

--- startup/11-suspenders.py
+++ startup/11-suspenders.py
@@ -1,6 +1,6 @@
 from bluesky.suspenders import SuspendBoolLow
 
 
-pps_shutter_enabled_suspender = SuspendBoolLow(EpicsSignalRO(pps_shutter.enabled_status.pvname))
-RE.install_suspender(pps_shutter_enabled_suspender)
+# pps_shutter_enabled_suspender = SuspendBoolLow(EpicsSignalRO(pps_shutter.enabled_status.pvname))
+# RE.install_suspender(pps_shutter_enabled_suspender)
 
