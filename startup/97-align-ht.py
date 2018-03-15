419f64a7e227f6a9d8ddec2e463b49ff995bfb3f
Maksim Rakitin <mrakitin@bnl.gov>
2018-03-15 16:56:50 -0400

    Implemented opening of PPS shutter

--- startup/97-align-ht.py
+++ startup/97-align-ht.py
@@ -2,8 +2,8 @@ from bluesky.callbacks.mpl_plotting import plot_peak_stats
 from bluesky.callbacks.fitting import PeakStats
 
 
-HT_X_START = 12
-HT_Y_START = -90.2
+HT_X_START = 8.72
+HT_Y_START = -89.72
 HT_COORDS_FILE = str(PROFILE_STARTUP_PATH / 'ht_coords.csv')
 HT_COORDS_FILE_OLD = str(PROFILE_STARTUP_PATH / 'ht_coords_old.csv')
 LOAD_POS_X = -90
@@ -17,7 +17,7 @@ def align_ht(x_start=HT_X_START, y_start=HT_Y_START, md=None, offset=3, run=True
         y_start : vertical start position
         md : optional metadata information
     """
-    global PS_X, PS_Y, HT_COORDS, HT_COORDS_OLD
+    global HT_COORDS, HT_COORDS_OLD
     if run:
         def close_shutters():
             yield from bps.mv(shutter, 'Close')
@@ -25,7 +25,9 @@ def align_ht(x_start=HT_X_START, y_start=HT_Y_START, md=None, offset=3, run=True
             yield from bps.mv(ht.x, LOAD_POS_X, ht.y, LOAD_POS_Y)  # load position
 
         def main_plan():
+            global PS_X, PS_Y
             yield from bps.mv(ht.x, x_start-offset, ht.y, y_start)
+            yield from bps.mv(pps_shutter, 'Open')
 
             # Find uid and peak stats for horizontal calibration:
             uid, PS_X = yield from _align_ht('horizontal', ht.x,
