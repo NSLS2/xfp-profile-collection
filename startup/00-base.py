989e6d6659cdb5ff5790617518584f299f5d7f41
Maksim Rakitin <mrakitin@bnl.gov>
2018-07-02 15:53:23 -0400

    Enable BEC callback

--- startup/00-base.py
+++ startup/00-base.py
@@ -7,10 +7,15 @@ import nslsii
 import bluesky.plans as bp
 import bluesky.plan_stubs as bps
 import bluesky.preprocessors as bpp
+from bluesky.callbacks.best_effort import BestEffortCallback
 
 
 # Disable Best Effort Callback at the moment (01/18/2018):
 nslsii.configure_base(get_ipython().user_ns, 'xfp', bec=False)
+
+# Best effort callback to print a table and show a live plot
+bec = BestEffortCallback()
+RE.subscribe(bec)
 
 # nice format string to use in various places
 _time_fmtstr = '%Y-%m-%d %H:%M:%S'
