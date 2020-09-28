b03e1f54fed8cd07a03c3e66409ccbf9775f2fde
Erik Farquhar <efarquhar@bnl.gov>
2020-09-28 17:02:34 -0400

    Informative renaming of x-plan.py files for ES:1 expts

---
+++ startup/91-tr-cf.py
@@ -0,0 +1,86 @@
+#NEEDS WORK - NOT FINISHED!!!!!
+
+import datetime
+
+def tr_pump(mix_flow_rate, mix_vol, mixing_time, exp_flow_rate, exp_vol, *, md=None):
+    '''Run time-resolved experiment
+
+    Parameters
+    ----------
+    mix_flow_rate : float
+        flow rate in mL/min for mixing
+
+    mix_vol : float
+        volume to push for mixing without exposure in mL
+
+    mixing_time : float
+        time in seconds for mixing prior to exposure
+
+    exp_flow_rate : float
+        flow rate in mL/min for exposure and collection
+
+    exp_vol : float
+        volume to collect with exposure in mL
+    ''' 
+    
+    dets = [shutter, spump]
+
+    if md is None:
+        md = {}
+
+    md['mix_flow_rate'] = mix_flow_rate
+    md['mix_vol'] = mix_vol
+    md['mixing_time'] = mixing_time
+    md['exp_flow_rate'] = exp_flow_rate
+    md['exp_vol'] = exp_vol
+
+    @bpp.run_decorator(md={'plan_name': 'tr_pump'})
+    def inner_plan():
+        yield from bps.clear_checkpoint()
+
+        # set pump diameter - MUST BE DONE ONCE IN THIS PROGRAM***
+        yield from bps.abs_set(spump.diameter, 14.57, wait=True)
+
+        # set pump parameters for mixing push
+        yield from bps.abs_set(spump.infusion_rate, mix_flow_rate, wait=True)
+        yield from bps.abs_set(spump.infusion_volume, mix_vol, wait=True)
+        
+        # start the pump for mixing
+        yield from bps.kickoff(spump)
+
+        print("== ({}) flowing at {} mL/m".format(datetime.datetime.now().strftime(_time_fmtstr), mix_flow_rate))
+        yield from bps.complete(spump, wait=True)
+        finish_time = time.time()
+        yield from bps.trigger_and_read(dets)
+        print("({}) first push complete, mixing for {:.2f} s".format(datetime.datetime.now().strftime(_time_fmtstr), mixing_time))
+        
+        # set pump parameters for exposure push
+        yield from bps.abs_set(spump.infusion_rate, exp_flow_rate, wait=True)
+        yield from bps.abs_set(spump.infusion_volume, exp_vol, wait=True)
+
+        # mix
+        delay = time.time() - finish_time
+        #print('delay': delay)
+        yield from bps.sleep(max(0, (mixing_time - (delay))))
+
+        # open the shutter
+        yield from bps.abs_set(shutter, 'Open', wait=True)
+        yield from bps.trigger_and_read(dets)
+
+        # start the pump for exposure and collection
+        yield from bps.kickoff(spump, wait=True)
+
+        print("== ({}) exposing sample at {} mL/m".format(datetime.datetime.now().strftime(_time_fmtstr), exp_flow_rate))
+        print('waiting for pump to finish')
+        yield from bps.complete(spump, wait=True)
+        
+        # close the shutter
+        yield from bps.abs_set(shutter, 'Close', wait=True)
+        print('closed shutter')
+        print("== ({}) ***Exposure Complete!***".format(datetime.datetime.now().strftime(_time_fmtstr)))
+
+    def clean_up():
+        yield from bps.abs_set(shutter, 'Close', wait=True)
+
+    yield from bpp.finalize_wrapper(inner_plan(), clean_up())
+
---
+++ startup/93-capflow.py
@@ -0,0 +1,63 @@
+#syringe pump capillary flow experiment
+
+import datetime
+
+def flow(diameter, rate, tgt_vol, *, md=None):
+
+    dets = [shutter, spump]
+
+    if md is None:
+        md = {}
+
+    md['diameter'] = diameter
+    md['rate'] = rate
+    md['tgt_vol'] = tgt_vol
+
+    @bpp.run_decorator(md={'plan_name': 'flow'})
+    def inner_plan():
+        yield from bps.clear_checkpoint()
+
+        # set pump parameters
+        yield from bps.abs_set(spump.diameter, diameter, wait=True)
+        yield from bps.abs_set(spump.infusion_rate, rate, wait=True)
+        yield from bps.abs_set(spump.infusion_volume, tgt_vol, wait=True)
+
+        # open the shutter
+        yield from bps.abs_set(shutter, 'Open', wait=True)
+        yield from bps.trigger_and_read(dets)
+
+        print('Shutter opened')
+ 
+        # push sample
+        print('waiting for pump to start')
+        #yield from bps.kickoff(spump, wait=True)
+        #for rate test yield from bps.sleep(10)
+        yield from bps.kickoff(spump, group='pump_started')
+        yield from bps.wait('pump_started')
+        print("({}) Exposing {:.2f} mL at {:.2f} mL/min".format(datetime.datetime.now().strftime(_time_fmtstr), tgt_vol, rate))
+        print('waiting for pump to finish')
+        st = yield from bps.complete(spump, group='pump_done', wait=False)
+        #st = yield from bps.complete(spump, wait=True)
+        #print('pump finished')
+        yield from bps.trigger_and_read(dets)
+
+        while st is not None and not st.done:
+            yield from bps.trigger_and_read(dets)
+            yield from bps.sleep(.5)
+
+        yield from bps.sleep(.1)
+        yield from bps.trigger_and_read(dets)
+
+        yield from bps.wait('pump_done')
+        print('pump finished')
+        
+       # close the shutter
+        yield from bps.abs_set(shutter, 'Close', wait=True)
+        print('closed shutter')
+        yield from bps.trigger_and_read(dets)
+
+    def clean_up():
+        yield from bps.abs_set(shutter, 'Close', wait=True)
+
+    yield from bpp.finalize_wrapper(inner_plan(), clean_up())
+
