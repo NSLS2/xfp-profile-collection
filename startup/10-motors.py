9ef27754c5faa5f18cd1309ce02d3650215ae47e
Erik Farquhar <efarquhar@bnl.gov>
2021-07-15 18:58:18 -0400

    Added ES:3 motion components

--- startup/10-motors.py
+++ startup/10-motors.py
@@ -1,6 +1,7 @@
 #Defines classes for beamline / endstation motors
-#Includes: all components in XFP PDS, ES:1, and ES:2
-#Excludes: ES:3, front end slits, and front end mirror
+#Includes: all components in XFP PDS, ES:1, and ES:3.
+#ES:3 XAS components are defined in a separate section at bottom
+#Excludes: front end slits and front end mirror
 
 #This stuff should be imported by 10-fp-devices.py
 import time
@@ -92,4 +93,43 @@ class ADCSlits(Device):
 
 adcslits = ADCSlits('XF:17BMA-OP{Slt:ADC-Ax:', name='adcslits')
 
+#XAS components at ES:3. 
+#Includes: monochromator, cryo stage, premono slits, and table
+#Excludes: picomotor and PB_Diag_Y
+
+#XAS monochromator in keV energy and theta units in a single class.
+#Units on xasmono.en are in keV.
+class XASMono(Device):
+    en = Cpt(EpicsMotor, 'En}Mtr', settle_time=0.5)
+    theta = Cpt(EpicsMotor, 'Theta}Mtr', settle_time=0.5)
+
+xasmono = XASMono('XF:17BMA-OP{Mono:1-Ax:', name='xasmono')
+
+#Sample cryostat motion stages, three axes:
+class Sample_Cryo(Device):
+    x = Cpt(EpicsMotor, 'X}Mtr')
+    y = Cpt(EpicsMotor, 'Y}Mtr')
+    z = Cpt(EpicsMotor, 'Z}Mtr')
+
+sample_cryo = Sample_Cryo('XF:17BMA-ES:3{Stg:9-Ax:', name='sample_cryo')
+
+#Real and virtual PreMono Slit axes in a single class.
+#Functionally identical to the XFP PB slits
+class PreMonoSlits(Device):
+    top = Cpt(EpicsMotor, 'T}Mtr')
+    bot = Cpt(EpicsMotor, 'B}Mtr')
+    inb = Cpt(EpicsMotor, 'I}Mtr')
+    outb = Cpt(EpicsMotor, 'O}Mtr')
+    xgap = Cpt(EpicsMotor, 'XGap}Mtr')
+    xctr = Cpt(EpicsMotor, 'XCtr}Mtr')
+    ygap = Cpt(EpicsMotor, 'YGap}Mtr')
+    yctr = Cpt(EpicsMotor, 'YCtr}Mtr')
+
+premono_slits = PreMonoSlits('XF:17BMA-OP{Slt:PB-Ax:', name='premono_slits')
+
+#ES:3 vertical lift table
+class Table3(Device):
+    y = Cpt(EpicsMotor, 'Y}Mtr')
+
+tbl3 = Table3('XF:17BMA-ES:3{Tb3:1-Ax:', name='tbl3')
 
