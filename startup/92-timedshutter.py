27d5f46be7a71d37b12be6ad249a327a8f177131
Erik Farquhar <efarquhar@bnl.gov>
2022-01-25 15:08:46 -0500

    Add docstrings for timed shutter funcs

--- startup/92-timedshutter.py
+++ startup/92-timedshutter.py
@@ -3,7 +3,14 @@
 import datetime
 
 def timed_shutter(exp_time, *, md=None):
+    '''Opens the pre-shutter for a defined exposure time.
+    Prerequisites: FE photon shutter and any downstream shutters are open
 
+    Parameter
+    ---------
+    exp_time: float
+        Exposure time in seconds
+    '''
     dets = [shutter]
 
     if md is None:
@@ -39,6 +46,14 @@ def timed_shutter(exp_time, *, md=None):
 #Functions to actuate Uniblitz fast shutter
 
 def timed_uniblitz(fire_time):
+    '''Opens the Uniblitz shutter for a defined exposure time.
+    Prerequisites: FE photon shutter and pre-shutter are open
+
+    Parameter
+    ---------
+    fire_time: float
+        Exposure time in seconds
+    '''
     yield from bps.mv(dg, fire_time)        #set Uniblitz opening time
     yield from bps.mv(dg.fire, 1)           #fire Uniblitz
     yield from bps.sleep(fire_time*1.1)     #wait for shutter to finish
@@ -49,7 +64,15 @@ def timed_uniblitz(fire_time):
         fire_time_str = str(fire_time)
         print("Fired Uniblitz shutter for " + fire_time_str + " seconds.")
 
-def timed_uniblitz_pre(fire_time):          #Actuate pre-shutter, then uniblitz fast shutter
+def timed_uniblitz_pre(fire_time):
+    '''Opens the pre-shutter, then Uniblitz shutter for a defined exposure time.
+    Prerequisites: FE photon shutter is open
+
+    Parameter
+    ---------
+    fire_time: float
+        Exposure time in seconds
+    '''
     yield from bps.mv(shutter, 'Open')      #open pre-shutter
     print("Opened pre-shutter.")
     if fire_time <= 0.5:                    #add a pre-shutter sleep for short exposure times
@@ -60,7 +83,15 @@ def timed_uniblitz_pre(fire_time):          #Actuate pre-shutter, then uniblitz 
     yield from bps.mv(shutter, 'Close')     #close pre-shutter
     print("Closed pre-shutter.")
 
-def timed_uniblitz_ss(fire_time):                   #Func. to actuate sample shutter, then uniblitz fast shutter
+def timed_uniblitz_ss(fire_time):
+    '''Opens the sample shutter, then Uniblitz shutter for a defined exposure time.
+    Prerequisites: FE photon shutter and pre-shutter are open
+
+    Parameter
+    ---------
+    fire_time: float
+        Exposure time in seconds
+    '''
     yield from bps.mv(diode_shutter, 'open')
     print("Opened DIODE sample shutter.")
     if fire_time <= 0.5:                            #sample-shutter sleep for short exposure times   
@@ -73,7 +104,15 @@ def timed_uniblitz_ss(fire_time):                   #Func. to actuate sample shu
 
 #Functions to actuate sample shutter in BIFS
 
-def timed_sam_shutter(ss_exp_time):                 #Actuate sample shutter for specified time
+def timed_sam_shutter(ss_exp_time):
+    '''Opens the sample shutter for a defined exposure time.
+    Prerequisites: FE photon shutter and pre-shutter are open
+
+    Parameter
+    ---------
+    ss_exp_time_time: float
+        Exposure time in seconds
+    '''
     yield from bps.mv(diode_shutter, 'open')
     print("Opened DIODE sample shutter.")
     yield from bps.sleep(ss_exp_time)
@@ -81,7 +120,15 @@ def timed_sam_shutter(ss_exp_time):                 #Actuate sample shutter for 
     ss_exp_time_str = str(ss_exp_time)
     print("Closed DIODE sample shutter after a " + ss_exp_time_str + " second(s) exposure.")
 
-def timed_sam_shutter_pre(ss_exp_time):             #Actuate pre-shutter, then sample shutter for specified time
+def timed_sam_shutter_pre(ss_exp_time):
+    '''Opens the pre-shutter, then sample shutter for a defined exposure time.
+    Prerequisites: FE photon shutter is open
+
+    Parameter
+    ---------
+    ss_exp_time_time: float
+        Exposure time in seconds
+    '''
     yield from bps.mv(shutter, 'Open')
     print("Opened pre-shutter.")
     yield from timed_sam_shutter(ss_exp_time)
