89082379f04e608373658af734e4a07140c31619
Jun Aishima <jaishima@bnl.gov>
2022-04-25 10:07:59 -0400

    Update .ci/bl-specific.sh
    Co-authored-by: Maksim Rakitin <mrakitin@users.noreply.github.com>

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -2,3 +2,5 @@
 
 sudo mkdir -p /etc/bluesky
 sudo touch /etc/bluesky/kafka.yml
+
+TODO: add a realistic config file so that `nslsii.configure_base(..., publish_documents_with_kafka=True)` does not fail.
