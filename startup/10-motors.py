a942c3e072fa4037a2165a6a6c27fd64383c89ef
XFP Operator <xf17bm@xf17bm-ws1.nsls2.bnl.local>
2022-05-24 14:10:41 -0400

    Add front-end motion components for baseline tracking

--- startup/10-motors.py
+++ startup/10-motors.py
@@ -133,3 +133,32 @@ class Table3(Device):
 
 tbl3 = Table3('XF:17BMA-ES:3{Tb3:1-Ax:', name='tbl3')
 
+
+#Define front-end motors for baseline purposes
+#FE slits limited to the physical X/Y positions for now
+
+class FE_WhiteBeam_Slits(Device):
+    top = Cpt(EpicsMotor, '1-Ax:T}Mtr')
+    bot = Cpt(EpicsMotor, '2-Ax:B}Mtr')
+    inb = Cpt(EpicsMotor, '2-Ax:I}Mtr')
+    outb = Cpt(EpicsMotor, '1-Ax:O}Mtr')
+
+fe_wb_slits = FE_WhiteBeam_Slits('FE:C17B-OP{Slt:', name='fe_wb_slits')
+
+class XFP_FE_Mirror(Device):
+    hor_up = Cpt(EpicsMotor, '-Ax:XU}Mtr')
+    hor_down = Cpt(EpicsMotor, '-Ax:XD}Mtr')
+    lift_up = Cpt(EpicsMotor, '-Ax:YUI}Mtr')
+    lift_ctr = Cpt(EpicsMotor, '-Ax:YO}Mtr')
+    lift_down = Cpt(EpicsMotor, '-Ax:YDI}Mtr')
+    focus = Cpt(EpicsMotor, '-Ax:Bend}Mtr')
+    X = Cpt(EpicsMotor, '-Ax:X}Mtr')
+    Y = Cpt(EpicsMotor, '-Ax:Y}Mtr')
+    pitch = Cpt(EpicsMotor, '-Ax:P}Mtr')
+    yaw = Cpt(EpicsMotor, '-Ax:Yaw}Mtr')
+    roll = Cpt(EpicsMotor, '-Ax:P}Mtr')
+    temp1 = Cpt(EpicsSignalRO, '}T:1-I')
+    temp2 = Cpt(EpicsSignalRO, '}T:2-I')
+
+xfp_fe_mirror = XFP_FE_Mirror('XF:17BM-OP{Mir:1', name='xfp_fe_mirror')
+
