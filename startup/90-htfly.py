e34957dd88c195c28b43e57a58c61c685b52b1f9
XFP Operator <xf17bm@xf17bm-ws1.nsls2.bnl.local>
2022-08-12 18:24:45 -0400

    WIP HTFly Control: Enable metadata writing to databroker for each run

--- startup/90-htfly.py
+++ startup/90-htfly.py
@@ -18,7 +18,7 @@ def htfly_move_to_load():
         print("Already there!")
 
 
-def htfly_exp_row(row_num, htfly_vel, hslit_size, al_thickness):
+def htfly_exp_row(row_num, htfly_vel, hslit_size, al_thickness, *, md=None):
     '''
     Function to expose a single row on the HTFly device, specifying velocity, 
     slit size, and attenuation. Moves device back to load position after exposure.
@@ -41,94 +41,120 @@ def htfly_exp_row(row_num, htfly_vel, hslit_size, al_thickness):
     al_thickness: integer
         Aluminum attenuator thickness in um.
         Must be an entry in the list [762, 508, 305, 203, 152, 76, 25, 0]
-    
+
+    md: optional user specified metadata
+        By default, the plan name, exposure time, row number, stage velocity, 
+        slit size, and attenuator are written as metadaa for each successful run.
+     
     '''
 
-    #Calculate exposure time in milliseconds, rounded to 3 decimal places
-    htfly_exp_time = round((hslit_size / htfly_vel) * 1000, 3)
-    
-    #Set HTFly velocity, trap velocities that are negative or > 500
-    if htfly_vel <= 0:
-        raise ValueError(f"You entered {htfly_vel} mm/s. Enter a velocity greater than 0 mm/sec!")
-    if htfly_vel > 500:
-        raise ValueError(f"You entered {htfly_vel} mm/s. Enter a velocity less than or equal to 500 mm/sec!")
+    #Calculate exposure time in milliseconds, rounded to 3 decimal places.
+    #Trap velocity = 0 mistakes here.
+    if htfly_vel == 0:
+        raise ValueError(f"You entered {htfly_vel} mm/s. The stage won't move!")
     else:
-        print(f"Setting htfly_x stage velocity to {htfly_vel} mm/sec.")
-        yield from bps.mv(htfly.x.velocity, htfly_vel)
+        htfly_exp_time = round((hslit_size / htfly_vel) * 1000, 3)
+    
+    _md = {'plan_name': 'htfly_exp_row',
+           'exposure': htfly_exp_time,
+           'row_number': row_num,
+           'htfly_velocity': htfly_vel,
+           'slit_size': hslit_size,
+           'attenuator': al_thickness}
+    _md.update(md or {})
+
+    def htfly_exp_setup():
+        #Set HTFly velocity, trap velocities that are negative or > 500
+        if htfly_vel < 0:
+            raise ValueError(f"You entered {htfly_vel} mm/s. Enter a velocity greater than 0 mm/sec!")
+        if htfly_vel > 500:
+            raise ValueError(f"You entered {htfly_vel} mm/s. Enter a velocity less than or equal to 500 mm/sec!")
+        else:
+            print(f"Setting htfly_x stage velocity to {htfly_vel} mm/sec.")
+            yield from bps.mv(htfly.x.velocity, htfly_vel)
         
-    #Set ADC slit size, trap exception if a negative number is entered or value > 6 is entered
-    if hslit_size <= 0:
-        raise ValueError(f"You entered {hslit_size} mm slit size. Enter a positive horizontal slit size!")
-    elif hslit_size > 6:
-        raise ValueError(f"You entered {hslit_size} mm slit size. Enter a horizontal slit size smaller than 6 mm!")
-    else:        
-        print(f"Moving the ADC horizontal slit size to {hslit_size} mm.")
-        yield from bps.mv(adcslits.xgap, hslit_size)
+        #Set ADC slit size, trap exception if a negative number is entered or value > 6 is entered
+        if hslit_size <= 0:
+            raise ValueError(f"You asked for a {hslit_size} mm slit. Enter a positive horizontal slit size!")
+        elif hslit_size > 6:
+            raise ValueError(f"You asked for a {hslit_size} mm slit. Enter a horizontal slit size smaller than 6 mm!")
+        else:        
+            print(f"Moving the ADC horizontal slit size to {hslit_size} mm.")
+            yield from bps.mv(adcslits.xgap, hslit_size)
             
-    #Move to desired row number, throw exception if row /= 1-6
-    if row_num == 1:
-        print(f"moving to row 1, at htfly_y ={row3_y_vert-18}")
-        yield from bps.mv(htfly.y, row3_y_vert-18)
-    elif row_num == 2:
-        print(f"moving to row 2, at htfly_y ={row3_y_vert-9}")
-        yield from bps.mv(htfly.y, row3_y_vert-9)
-    elif row_num == 3:
-        print(f"moving to row 3, at htfly_y ={row3_y_vert}")
-        yield from bps.mv(htfly.y, row3_y_vert)
-    elif row_num == 4:
-        print(f"moving to row 4, at htfly_y ={row3_y_vert+9}")
-        yield from bps.mv(htfly.y, row3_y_vert+9)
-    elif row_num == 5:
-        print(f"moving to row 5, at htfly_y ={row3_y_vert+18}")
-        yield from bps.mv(htfly.y, row3_y_vert+18)
-    elif row_num == 6:
-        print(f"moving to row 6, at htfly_y ={row3_y_vert+27}")
-        yield from bps.mv(htfly.y, row3_y_vert+27)
-    else:
-        raise ValueError(f"You entered row {row_num}. Row value must be in the range 1 - 6!")
+        #Move to desired row number, throw exception if row /= 1-6
+        if row_num == 1:
+            print(f"moving to row 1 at htfly_y = {row3_y_vert-18}")
+            yield from bps.mv(htfly.y, row3_y_vert-18)
+        elif row_num == 2:
+            print(f"moving to row 2 at htfly_y = {row3_y_vert-9}")
+            yield from bps.mv(htfly.y, row3_y_vert-9)
+        elif row_num == 3:
+            print(f"moving to row 3 at htfly_y = {row3_y_vert}")
+            yield from bps.mv(htfly.y, row3_y_vert)
+        elif row_num == 4:
+            print(f"moving to row 4 at htfly_y = {row3_y_vert+9}")
+            yield from bps.mv(htfly.y, row3_y_vert+9)
+        elif row_num == 5:
+            print(f"moving to row 5 at htfly_y = {row3_y_vert+18}")
+            yield from bps.mv(htfly.y, row3_y_vert+18)
+        elif row_num == 6:
+            print(f"moving to row 6 at htfly_y = {row3_y_vert+27}")
+            yield from bps.mv(htfly.y, row3_y_vert+27)
+        else:
+            raise ValueError(f"You entered row {row_num}. Row value must be in the range 1 - 6!")
     
-    #Check the attenuator thickness is in the filter wheel list dictionary.
-    if not any(d['thickness'] == al_thickness for d in filter_wheel.wheel_positions):
-        raise ValueError(f"{al_thickness} is not an available attenuator. Choose from: 762, 508, 305, 203, 152, 76, 25, or 0")
-    else:
-        print("Moving filter wheel to " + str(al_thickness) + "um Al attenuation.")
-        yield from bps.mv(filter_wheel.thickness, al_thickness)
+        #Check the attenuator thickness is in the filter wheel list dictionary.
+        if not any(d['thickness'] == al_thickness for d in filter_wheel.wheel_positions):
+            raise ValueError(f"{al_thickness} is not an available attenuator. Choose from: 762, 508, 305, 203, 152, 76, 25, or 0")
+        else:
+            print("Moving filter wheel to " + str(al_thickness) + "um Al attenuation.")
+            yield from bps.mv(filter_wheel.thickness, al_thickness)
 
-    #Check that HTFly is at load position and move it there before opening shutters.
-    if htfly.x.position != LOAD_HTFLY_POS_X:
-        print("Moving to load position.")
-        yield from bps.mv(htfly.x, LOAD_HTFLY_POS_X)
+        #Check that HTFly is at load position and move it there before opening shutters.
+        if htfly.x.position != LOAD_HTFLY_POS_X:
+            print("Moving to load position.")
+            yield from bps.mv(htfly.x, LOAD_HTFLY_POS_X)
     
-    #Check state of pps_shutter and pre_shutter and open if needed and enabled.
-    #If the pps_shutter is disabled, exit and inform the user.
-    #This nomenclature allows the shutters to remain open after RE completes.
-    if EpicsSignalRO(pps_shutter.enabled_status.pvname).get() == 0:
-        raise Exception("Can't open photon shutter! Check that the hutch is interlocked and the shutter is enabled.")
+        #Check state of pps_shutter and pre_shutter and open if needed and enabled.
+        #If the pps_shutter is disabled, exit and inform the user.
+        #This nomenclature allows the shutters to remain open after RE completes.
+        if EpicsSignalRO(pps_shutter.enabled_status.pvname).get() == 0:
+            raise Exception("Can't open photon shutter! Check that the hutch is interlocked and the shutter is enabled.")
     
-    if pps_shutter.status.get() == 'Not Open':
-        print("The photon shutter was closed and is now being opened.")
-        pps_shutter.set('Open')
-        yield from bps.sleep(3)   #Allow some wait time for the shutter opening to finish
+        if pps_shutter.status.get() == 'Not Open':
+            print("The photon shutter was closed and is now being opened.")
+            pps_shutter.set('Open')
+            yield from bps.sleep(3)   #Allow some wait time for the shutter opening to finish
         
-    if pre_shutter.status.get() == 'Not Open':
-        print("The pre-shutter was closed and is now being opened.")
-        pre_shutter.set('Open')
-        yield from bps.sleep(3)   #Allow some wait time for the shutter opening to finish
+        if pre_shutter.status.get() == 'Not Open':
+            print("The pre-shutter was closed and is now being opened.")
+            pre_shutter.set('Open')
+            yield from bps.sleep(3)   #Allow some wait time for the shutter opening to finish
+   
+    @bpp.run_decorator(md=_md)
+    def inner_htfly_exp():
+        
+        #Run the setup plan
+        yield from htfly_exp_setup()
 
-    print("Pre-shutter and PPS shutter are open. Opening the sample shutter and Uniblitz.")
+        #Open sample shutter and do the exposure.
+        print("Pre-shutter and PPS shutter are open. Opening the sample shutter and Uniblitz.")
   
-    yield from bps.mv(diode_shutter, 'open')
-    yield from bps.mv(dg, 30)               #set Uniblitz opening time
-    yield from bps.mv(dg.fire, 1)           #fire Uniblitz
+        yield from bps.mv(diode_shutter, 'open')
+        yield from bps.mv(dg, 30)               #set Uniblitz opening time
+        yield from bps.mv(dg.fire, 1)           #fire Uniblitz
     
-    print(f"\nExposing row {row_num} at {htfly_vel}mm/sec with a {hslit_size}mm horizontal slit and {al_thickness}um Al attenuation.")
-    print(f"This corresponds to an exposure time of {htfly_exp_time} milliseconds.\n")
-    yield from bps.mv(htfly.x, EXPOSED_HTFLY_POS_X)
+        print(f"\nExposing row {row_num} at {htfly_vel}mm/sec with a {hslit_size}mm horizontal slit and {al_thickness}um Al attenuation.")
+        print(f"This corresponds to an exposure time of {htfly_exp_time} milliseconds.\n")
+        yield from bps.mv(htfly.x, EXPOSED_HTFLY_POS_X)
     
-    #Cleanup: close shutters, return to load position.
-    print("Closing sample shutter and returning to load position")
-    yield from bps.mv(diode_shutter, 'close')
-    yield from bps.mv(dg, 0)    #Close Uniblitz shutter after exposure
-    yield from bps.sleep(1)
-    yield from bps.mv(htfly.x, LOAD_HTFLY_POS_X)
-    print("All done, ready for another row!")
+        #Cleanup: close shutters, return to load position.
+        print("Closing sample shutter and returning to load position")
+        yield from bps.mv(diode_shutter, 'close')
+        yield from bps.mv(dg, 0)    #Close Uniblitz shutter after exposure
+        yield from bps.sleep(1)
+        yield from bps.mv(htfly.x, LOAD_HTFLY_POS_X)
+        print("All done, ready for another row!")
+
+    return (yield from inner_htfly_exp())
