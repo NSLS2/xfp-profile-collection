8deed1ea2a0760283c37b86da37ea2517c3b53ef
XFP Operator <xf17bm@xf17bm-ws1.nsls2.bnl.local>
2022-12-20 13:34:36 -0500

    updates to timed shutter funcs, new alignment pos

--- startup/92-timedshutter.py
+++ startup/92-timedshutter.py
@@ -2,7 +2,7 @@
 
 import datetime
 
-def timed_shutter(exp_time, *, md=None):
+def timed_shutter_pre(exp_time, *, md=None):
     '''Opens the pre-shutter for a defined exposure time.
     Prerequisites: FE photon shutter and any downstream shutters are open
 
@@ -18,15 +18,19 @@ def timed_shutter(exp_time, *, md=None):
 
     md['exp_time'] = exp_time
 
-    @bpp.run_decorator(md={'plan_name': 'timed_shutter'})
+    @bpp.run_decorator(md={'plan_name': 'timed_shutter_pre'})
     def inner_plan():
         yield from bps.clear_checkpoint()
         
+        #check state of FE shutter
+        if pps_shutter.status.get() == 'Not Open' and EpicsSignalRO(pps_shutter.enabled_status.pvname).get() == 1:
+            raise Exception("The photon shutter is not open. Open it first!")
+
         # open the shutter
         yield from bps.abs_set(shutter, 'Open', wait=True)
         yield from bps.trigger_and_read(dets)
 
-        print('Shutter opened')
+        print('Pre-shutter opened')
         print("({}) Exposing for {:.2f} s".format(datetime.datetime.now().strftime(_time_fmtstr), exp_time))
 
        # wait
@@ -34,7 +38,7 @@ def timed_shutter(exp_time, *, md=None):
 
         # close the shutter
         yield from bps.abs_set(shutter, 'Close', wait=True)
-        print('closed shutter')
+        print('closed pre-shutter')
         yield from bps.trigger_and_read(dets)
 
     def clean_up():
@@ -43,6 +47,56 @@ def timed_shutter(exp_time, *, md=None):
 
     yield from bpp.finalize_wrapper(inner_plan(), clean_up())
 
+def timed_shutter(exp_time, *, md=None):
+    '''Opens the PPS shutter and pre-shutter for a defined exposure time.
+    Prerequisites: Any other downstream shutters are open
+
+    Parameter
+    ---------
+    exp_time: float
+        Exposure time in seconds
+    '''
+    dets = [pps_shutter, shutter]
+
+    if md is None:
+        md = {}
+
+    md['exp_time'] = exp_time
+
+    @bpp.run_decorator(md={'plan_name': 'timed_shutter'})
+    def inner_plan():
+        yield from bps.clear_checkpoint()
+        
+        # open the PPS and preshutters
+        if EpicsSignalRO(pps_shutter.enabled_status.pvname).get() == 0:
+            raise Exception("Can't open FE shutter! Check that the hutch is interlocked and the shutter is enabled.")
+        
+        yield from bps.abs_set(pps_shutter, 'Open', wait=True)
+        print('FE shutter opened')
+        
+        yield from bps.abs_set(shutter, 'Open', wait=True)
+        yield from bps.trigger_and_read(dets)
+
+        print('Pre-shutter opened')
+        print("({}) Exposing for {:.2f} s".format(datetime.datetime.now().strftime(_time_fmtstr), exp_time))
+
+       # wait
+        yield from bps.sleep(exp_time)
+
+        # close the shutters
+        yield from bps.abs_set(shutter, 'Close', wait=True)
+        print('closed pre-shutter')
+        yield from bps.abs_set(pps_shutter, 'Close', wait=True)
+        print('closed FE shutter')
+        yield from bps.trigger_and_read(dets)
+
+    def clean_up():
+        yield from bps.abs_set(shutter, 'Close', wait=True)
+        yield from bps.abs_set(pps_shutter, 'Close', wait=True)
+
+    yield from bpp.finalize_wrapper(inner_plan(), clean_up())
+
+#TODO: fix up functions below to capture into databroker.
 #Functions to actuate Uniblitz fast shutter
 
 def timed_uniblitz(fire_time):
@@ -58,11 +112,9 @@ def timed_uniblitz(fire_time):
     yield from bps.mv(dg.fire, 1)           #fire Uniblitz
     yield from bps.sleep(fire_time*1.1)     #wait for shutter to finish
     if fire_time <= 1:
-        fire_time_str = str(fire_time * 1000)
-        print("Fired Uniblitz shutter for " + fire_time_str + " millseconds.")
+        print(f"Fired Uniblitz shutter for {fire_time*1000} millseconds.")
     else:
-        fire_time_str = str(fire_time)
-        print("Fired Uniblitz shutter for " + fire_time_str + " seconds.")
+        print(f"Fired Uniblitz shutter for {fire_time} seconds.")
 
 def timed_uniblitz_pre(fire_time):
     '''Opens the pre-shutter, then Uniblitz shutter for a defined exposure time.
@@ -117,8 +169,7 @@ def timed_sam_shutter(ss_exp_time):
     print("Opened DIODE sample shutter.")
     yield from bps.sleep(ss_exp_time)
     yield from bps.mv(diode_shutter, 'close')
-    ss_exp_time_str = str(ss_exp_time)
-    print("Closed DIODE sample shutter after a " + ss_exp_time_str + " second(s) exposure.")
+    print(f"Closed DIODE sample shutter after a {ss_exp_time} second(s) exposure.")
 
 def timed_sam_shutter_pre(ss_exp_time):
     '''Opens the pre-shutter, then sample shutter for a defined exposure time.
