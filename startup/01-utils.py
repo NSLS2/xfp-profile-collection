52e7a7c752abbb2e101f02bfb1fd9a64638000da
Erik Farquhar <efarquhar@bnl.gov>
2023-06-07 16:35:32 -0400

    Funcs to collect persistant metadata for users

--- startup/01-utils.py
+++ startup/01-utils.py
@@ -3,3 +3,32 @@ def start_bec():
     bec = BestEffortCallback()
     RE.subscribe(bec)
     print("BestEffortCallback enabled!")
+
+def set_user_md():
+    '''
+    Function to define user / experiment values as persistant metadata
+    Collect using input at the prompt.
+    '''
+    proposal_num = input("Enter the proposal number: ")
+    saf_num = input("Enter the SAF number: ")
+    pi_name = input("Enter last name of the PI: ")
+    user_name = input("Enter last name of the lead experimenter: ")
+    RE.md['proposal'] = proposal_num
+    RE.md['SAF'] = saf_num
+    RE.md['PI'] = pi_name
+    RE.md['experimenter'] = user_name
+    print(f"\nSet proposal number to {proposal_num} and SAF number to {saf_num}.")
+    print(f"Set the PI to {pi_name} and lead experimenter to {user_name}.")
+
+def clear_user_md():
+    '''Clear user metadata, e.g. for commissioning with no proposal number'''
+    inp_choice = input("Are you sure you want to clear metadata (y/n)? ")
+    if inp_choice=='y' or inp_choice=='yes':
+        del RE.md['proposal']
+        del RE.md['SAF']
+        del RE.md['PI']
+        del RE.md['experimenter']
+        print("Cleared metadata keys for proposal, SAF, PI, and experimenter")
+    else:
+        print("\nNo change made! Current persistant metadata is:")
+        print(RE.md)
