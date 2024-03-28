b81ab3a6de85820a0de65d9d8a312b9930033e1f
Erik Farquhar <efarquhar@bnl.gov>
2024-03-28 15:14:33 -0400

    nsls2api query report + api-enhanced set_user_md()

--- startup/02-proposal-utils.py
+++ startup/02-proposal-utils.py
@@ -15,10 +15,6 @@ def get_from_api(url):
 
 def get_proposals_from_cycle(cycle):
     return get_from_api(f"proposals/{cycle}")
-def get_usernames_from_proposal(proposal_id):
-    return set(get_from_api(f"proposal/{proposal_id}/usernames")['usernames'])
-def get_users_from_proposal(proposal_id):
-    return get_from_api(f"proposal/{proposal_id}/users")
 def get_proposal_info(proposal_id):
     return get_from_api(f"proposal/{proposal_id}")
 
@@ -30,3 +26,84 @@ def get_proposals_for_instrument(cycle, instrument):
         if instrument in proposal['instruments']:
             proposals_on_instrument.append(proposal_num)
     return proposals_on_instrument
+
+def inst_proposals_report(cycle, instrument, detail):
+    '''
+    Queries NSLS-II API for proposals for specified instrument and cycle.
+    Detail is either 'simple' or 'full'
+    Returns report listing proposal number, title, type, and PI
+    '''   
+    print(f"Retrieving {instrument} proposals for the {cycle} cycle. This may take some time.")
+    proposal_list = sorted(get_proposals_for_instrument(cycle, instrument), key=int)
+    
+    if detail == 'long':
+        print(f"\nLong Report of {instrument} proposals for {cycle} cycle")
+        for item in proposal_list:
+            single_prop = get_proposal_info(item)
+            print("\nProposal #:", single_prop['proposal_id'], " Title:", single_prop['title'])
+            print("Proposal Type:", single_prop['type'])
+            pi_users = [user for user in single_prop['users'] if user['is_pi']]
+            for i, user in enumerate(pi_users):
+                print(f"Proposal PI: {user['first_name']} {user['last_name']}", end="")
+                if i < len(pi_users) - 1:
+                    print(",", end=" ")
+            print("\nUsers:", end=" ")
+            sorted_users = sorted(single_prop['users'], key=lambda x: x['last_name'])
+            for i, user in enumerate(sorted_users):
+                print(f"{user['first_name']} {user['last_name']} ({user['username']} / {user['bnl_id']})", end="")
+                if i < len(sorted_users) - 1:
+                    print(",", end=" ")
+            print(" ")
+    
+    if detail == 'medium':
+        print(f"\nMedium Report of {instrument} proposals for {cycle} cycle")
+        for item in proposal_list:
+            single_prop = get_proposal_info(item)
+            print("\nProposal #:", single_prop['proposal_id'], " Title:", single_prop['title'])
+            print("Proposal Type:", single_prop['type'])
+            #sometimes this prints out multiple entries for same PI due to data source
+            pi_users = [user for user in single_prop['users'] if user['is_pi']]
+            for i, user in enumerate(pi_users):
+                print(f"Proposal PI: {user['first_name']} {user['last_name']}", end="")
+                if i < len(pi_users) - 1:
+                    print(",", end=" ")
+            print(" ")
+    
+    if detail == 'short':
+        print(f"\nShort Report of {instrument} proposals for {cycle} cycle")
+        for item in proposal_list:
+            single_prop = get_proposal_info(item)
+            print("Proposal #:", single_prop['proposal_id'], " Title:", single_prop['title'])
+
+#Define metadata for user, proposal, and SAF while checking the API
+def set_user_md_api():
+    '''
+    Function to define user / experiment values as persistant metadata
+    Retrieve info from NSLS-II API based on collected proposal_id.
+    '''
+    short_report = input("Retrieve list of proposals (y/n)? ")
+    if short_report == 'y':
+        cycle_num = input("Which cycle (e.g. '2024-1')? ")
+        inst_proposals_report(cycle=cycle_num, instrument='XFP', detail='short')
+        print(" ")
+    proposal_num = input("Enter the proposal number: ")
+    single_prop = get_proposal_info(proposal_num)
+    print("\nProposal #:", single_prop['proposal_id'], " Title:", single_prop['title'])
+    approved_saf_ids = [saf['saf_id'] for saf in single_prop['safs'] if saf['status'] == 'APPROVED']
+    print("Approved SAFs:", ", ".join(approved_saf_ids))
+    sorted_users = sorted(single_prop['users'], key=lambda x: x['last_name'])
+    print("Proposal Members:")
+    for i, user in enumerate(sorted_users):
+        print(f"\n {user['first_name']} {user['last_name']} ({user['username']} / {user['bnl_id']})", end="")
+        if i < len(sorted_users) - 1:
+            print(",", end=" ")
+    print(" ")
+    saf_num = input("\nEnter the SAF number: ")
+    pi_name = input("Enter last name of the PI: ")
+    user_name = input("Enter last name of the lead experimenter: ")
+    RE.md['proposal'] = proposal_num
+    RE.md['SAF'] = saf_num
+    RE.md['PI'] = pi_name
+    RE.md['experimenter'] = user_name
+    print(f"\nSet proposal number to {proposal_num} and SAF number to {saf_num}.")
+    print(f"Set the PI to {pi_name} and lead experimenter to {user_name}.")
