9600a0f9c06c9e1daa07893dae725302b1a2e08c
Erik Farquhar <efarquhar@bnl.gov>
2024-07-01 10:23:15 -0400

    Minor tweaks to alignment func + docstrings

--- startup/89-align-htfly.py
+++ startup/89-align-htfly.py
@@ -9,13 +9,13 @@ def _htfly_align(mtr):
     '''
     Internal function to align single axis of htfly, producing liveplots and
     returning peak statistics. Takes motor name + axis as input and assumes
-    use of qem1 channel3 as the alignment detector. Not called by users.
+    use of qem1_channel3 as the alignment detector. Not called by users.
     '''
     
     peaks = PeakStats(mtr.name, qem1.current3.mean_value.name)
     lp = LivePlot(qem1.current3.mean_value.name, mtr.name)
     uid = yield from bpp.subs_wrapper(bp.rel_scan([qem1], mtr, -4, 4, 41), [lp, peaks])
-    print(f'Found center for {mtr.name} at COM = {round(peaks.com, 3)} mm, FWHM = {round(peaks.fwhm, 3)} mm.')
+    print(f'Found {mtr.name} at COM = {round(peaks.com, 3)} mm, FWHM = {round(peaks.fwhm, 3)} mm.')
     
     if uid is not None:
         if peaks['com'] is not None:
@@ -27,14 +27,13 @@ def _htfly_align(mtr):
 
 def htfly_align():
     '''
-    Alignment of htfly in both x and y axes. No variable input.
-    Assumes both pre-shutter and Uniblitz shutter are open.
-    Updates the ROW3_Y_VERT value if alignment succeeds.
-    If alignment is not run, uses ROW3_Y_VERT value set in startup.
+    Alignment of htfly in both x and y axes. Assumes both pre-shutter and Uniblitz shutter are open.
+    Updates the global ROW3_Y_VERT value if alignment succeeds.
+    If alignment is not run prior to htfly exposure plans, bluesky uses ROW3_Y_VERT value in startup.
     '''
     
     global ROW3_Y_VERT
-
+    print("Aligning HTFly apparatus in y, then x.")
     yield from bps.mv(htfly.y, HTFLY_Y_START, htfly.x, HTFLY_X_START)
     start_htflyx_vel = htfly.x.velocity.get()
     yield from bps.mv(htfly.x.velocity, 5)  #Slow x velocity for alignment purposes
@@ -58,6 +57,7 @@ def htfly_align():
     yield from bps.sleep(3)
     yield from bps.mv(htfly.x.velocity, start_htflyx_vel)
     yield from htfly_move_to_load()
+    print(f"Completed HTFly alignment, with row 3 center now set to {ROW3_Y_VERT} mm.")
 
 
 
